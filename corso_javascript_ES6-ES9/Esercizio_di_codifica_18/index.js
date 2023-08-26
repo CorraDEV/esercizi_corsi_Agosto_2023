c740eaa3e915dc18b41ad59ae8171666f7239457
corra <corrado.marzagalli@gmail.com>
2023-08-26 22:26:45 +0200

    finito esercizio di codifica 18

---
+++ corso_javascript_ES6-ES9/Esercizio_di_codifica_18/index.js
@@ -0,0 +1,15 @@
+function testUpperCase(variabile){
+    var res;
+    
+    res = variabile.toUpperCase();
+  
+    return res;
+}
+
+function testLowerCase(variabile){
+    var res;
+    
+    res = variabile.toLowerCase();
+  
+    return res;
+}
