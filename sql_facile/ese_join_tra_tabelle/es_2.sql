bd210985da3b0b906f91d55d2361c069ba83ca63
corra <corrado.marzagalli@gmail.com>
2023-09-06 21:13:06 +0200

    modificato es_2 in ese_join_tra_tabelle

--- sql_facile/ese_join_tra_tabelle/es_2.sql
+++ sql_facile/ese_join_tra_tabelle/es_2.sql
@@ -1,3 +1,3 @@
 SELECT nome, cognome, codice_fiscale, email, nome_mansione, ragione_sociale
 FROM dipendenti, mansioni, aziende
-WHERE dipendenti.id_mansione = mansioni.id AND dipendenti.id_mansione = aziende.id;
+WHERE dipendenti.id_mansione = mansioni.id AND dipendenti.id_azienda = aziende.id;
