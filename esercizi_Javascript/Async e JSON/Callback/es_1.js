f3bfd84f8e8f358cd0dbbfed262013e71d6bfe6c
Diabloking02 <corrado.marzagalli@gmail.com>
2023-09-21 23:44:21 +0200

    modificato es 1 callback

--- esercizi_Javascript/Async e JSON/Callback/es_1.js
+++ esercizi_Javascript/Async e JSON/Callback/es_1.js
@@ -1,22 +1,9 @@
-function somma(num1, num2){
-    return num1 + num2;
+function funzioneCallback(){
+    console.log('sono una funzione di callback');
 }
 
-function differenza(num1, num2){
-    return num1 - num2;
+function funzioneConCallback(callback){
+    callback();
 }
 
-function prodotto(num1, num2){
-    return num1 * num2;
-}
-
-function divisione(num1, num2){
-    return num1 / num2;
-}
-
-function operazione(num1, num2, operazioneFunc){
-    return operazioneFunc(num1, num2);
-}
-
-const res = operazione(10, 20, somma);
-console.log(res);
+funzioneConCallback(funzioneCallback);
