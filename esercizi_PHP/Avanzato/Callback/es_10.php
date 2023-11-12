d8a01825e28b0c32b38d18ba7a2059b2d99daa8d
corra <corrado.marzagalli@gmail.com>
2023-11-12 23:49:54 +0100

    modificato es 10 callback

--- esercizi_PHP/Avanzato/Callback/es_10.php
+++ esercizi_PHP/Avanzato/Callback/es_10.php
@@ -13,6 +13,6 @@ $diminuisci = function($arr){
     return $arr;
 };
 
-$nums = [1,2,3,4,5,6,7,8,9];
+$nums = ['a'=>1,'b'=>2,'c'=>3,'d'=>4,'e'=>5,'f'=>6,'g'=>7,'h'=>8,'i'=>9];
 stampaArrayModificato($nums, $diminuisci);
 ?>
